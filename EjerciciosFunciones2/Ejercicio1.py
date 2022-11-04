9fe3f8191678c069961b44fe593546a2745e5aa9
Gustavo Moya <gustavo.moya@bue.edu.ar>
2022-11-04 19:29:17 -0300

    Funciones recursivas. Ejercicio 1 modificado

--- EjerciciosFunciones2/Ejercicio1.py
+++ EjerciciosFunciones2/Ejercicio1.py
@@ -1,21 +1,48 @@
-def ejercicio1():
-    lista=[]
-    promedioSuc=0
-    totalVendido=0
-    nomSuc=input('Ingrese el nombre de la sucursal')
-    cantVentas=input('Ingrese la cantidad de ventas')
-    ventaSuc=input('Ingrese la venta total de la sucursal')
-    lista.append(nomSuc)
-    lista.append(cantVentas)
-    lista.append(ventaSuc)
-    promedioSuc=ventaSuc/cantVentas
+# Ejercicio 1.
+# Se deberá sacar el promedio de ventas en pesos de esa sucursal
 
-    if(nomSuc==0):
-        print('Sucursal:',nomSuc)
-        print('Cantidad de ventas',cantVentas)
-        print('Cantidad de ventas de la sucursal',cantVentas)
-        print('Promedio de cada sucursal:',cantVentas)
-def main():
-    ejercicio1()
-if __name__ == '__main__':
-    main()
+def ejercicio1() :
+    # Genere una lista vacía que luego se irá llenando según lo que ingrese el usuario.
+    datos = []
+
+    ingresarDatos(datos)
+
+    # Cuando el usuario ingrese un 0 como nombre de sucursal se deberá imprimir:
+    # - Todos los datos ingresados
+    # - El promedio de cada sucursal
+    # - El total vendido por toda la cadena
+    for dato in datos :
+        print('Nro Sucursal: ', dato[0])
+        print('Nombre: ', dato[1])
+        print('Cantidad de ventas: ', dato[2])
+        print('Total ventas: ', dato[3])
+        print('Venta promedio:', dato[4], '\n')
+
+# Pedir los siguientes datos:
+# - Nro de sucursal
+# - Nombre de sucursal
+# - Cantidad de ventas
+# - Venta total de la sucursal
+def ingresarDatos(datos) :
+    lista = []
+    nroSuc = input('Ingrese el nro de la sucursal: ')
+
+    if (int(nroSuc) == 0) :
+        return
+
+    nomSuc = input('Ingrese el nombre de la sucursal: ')
+    cantVtas = input('Ingrese la cantidad de ventas: ')
+    ventaSuc = input('Ingrese la venta total de la sucursal:')
+    promedioSuc = int(ventaSuc) / int(cantVtas)
+
+    lista.append(nroSuc)
+    lista.append(nomSuc)
+    lista.append(cantVtas)
+    lista.append(ventaSuc)
+    lista.append(promedioSuc)
+
+    datos.append(lista)
+
+    ingresarDatos(datos)
+
+ejercicio1()
